e522ccb5a56ef6ac577641afbaf27433cc14a56c
TeilzeitTaco <42586657+TeilzeitTaco@users.noreply.github.com>
2019-02-10 17:40:50 +0100

    cleanup
    Former-commit-id: 2fbf7e2cda27f712291ba6643bb6573dd1840a3b

--- PSP/FrontEnd.c
+++ PSP/FrontEnd.c
@@ -1,4 +1,8 @@
 #include <pspkernel.h>
+#include <pspctrl.h>
+#include <pspdebug.h>
+#include <pspgu.h>
+#include <psprtc.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,333 +11,290 @@
 #include <errno.h>
 #include <string.h>
 #include <time.h>
-#include <pspdebug.h>
 #include <math.h>
 #include <string.h>
-#include <pspctrl.h>
-#include <pspgu.h>
-#include <FrontEnd.h>
-#include <psprtc.h>
+
+#include "FrontEnd.h"
 #include "Version.h"
 
 typedef struct configparm{
-	char name[32];
-	long *var;
+		char name[32];
+		long *var;
 }configP;
 
-configP configparms[30];
-int totalconfig=0;
-
-
-void InitConfigParms(){
-	int c=0;
-
-    strcpy(configparms[c].name,"Screen <--> SWAP");
-	swap = configparms[c].var;
-	c++;
-	strcpy(configparms[c].name,"Show FPS");
-	showfps = configparms[c].var;
-	c++;
-	strcpy(configparms[c].name,"Enable Audio");
-	enable_sound = configparms[c].var;
-	c++;
-	strcpy(configparms[c].name,"frameskip");
-	frameskip = configparms[c].var;
-	c++;
-    strcpy(configparms[c].name,"language");
-	lang = configparms[c].var;
-	c++;
-
-	//
-	totalconfig =c;
-}
-
-int selposconfig=0;
-void DisplayConfigParms(){
-	int c;
-
-	for (c=0;c<totalconfig;c++){
-	 	
-	    if(selposconfig == c)
-		{
-			pspDebugScreenSetTextColor(0x0000ffff); // Yellow
-		}else {
-			pspDebugScreenSetTextColor(0xffffffff); // red
-		}
-		pspDebugScreenPrintf("  %s :  %d\n",configparms[c].name,configparms[c].var);
-		
-	}
-
-}
-
-int frameposconfig=0;
-int langposconfig=0;
-
-void DoConfig()
-{
-	int done =0;
-	SceCtrlData pad,oldPad;
-
-	pspDebugScreenSetXY(0,0);
-
-	int cnt;
-	for (cnt=0;cnt<100;cnt++)//pspDebugScreenPrintf("\n"); //?
-	while(!done){
-		sceDisplayWaitVblankStart();
-		pspDebugScreenSetTextColor(0xffffffff);
-		pspDebugScreenSetXY(0, 3);
-		pspDebugScreenPrintf("\n");
-		pspDebugScreenPrintf("\n");
-		pspDebugScreenPrintf("  DSonPSP configuration\n\n");
-		pspDebugScreenPrintf("  When you have finished your configuration, press START \n\n");
-		pspDebugScreenPrintf("  Lang config: 0 = JAP, 1 = ENG, 2 = FRE, 3 = GER,\n");
-		pspDebugScreenPrintf("  4 = ITA, 5 = SPA, 6 = CHI, 7 = RES\n");
-		pspDebugScreenPrintf("\n");
-		pspDebugScreenPrintf("\n");
-		InitConfigParms();
-		DisplayConfigParms();
-		if(sceCtrlPeekBufferPositive(&pad, 1))
-		{
-			if (pad.Buttons != oldPad.Buttons)
-			{
-                if(pad.Buttons & PSP_CTRL_LEFT)
-				{ 
-		   		if(strcmp(configparms[selposconfig].name,"language")== 0)
-				{
-				configparms[selposconfig].var = langposconfig--;
-				if(langposconfig == -1 )langposconfig = 0;
-				}else			
-				if(strcmp(configparms[selposconfig].name,"frameskip")== 0)
-				{
-				configparms[selposconfig].var = frameposconfig--;
-				if(frameposconfig == -1 )frameposconfig = 0;
-				}
-				else
-				{
-					configparms[selposconfig].var = 0;
-				 }
-				}
-				if(pad.Buttons & PSP_CTRL_RIGHT){
-				if(strcmp(configparms[selposconfig].name,"language")== 0)
-				{
-				configparms[selposconfig].var = langposconfig++;
-				if(langposconfig == 8 )langposconfig = 7;
-				}else
-				if(strcmp(configparms[selposconfig].name,"frameskip")== 0)
-				{
-				configparms[selposconfig].var = frameposconfig++;
-                if(frameposconfig == 10)frameposconfig = 9;  
-				}else{
-                configparms[selposconfig].var =1;
-				}
-				}
-				if(pad.Buttons & PSP_CTRL_START){
-					done = 1;//delay
-					break;
-				}
-				if(pad.Buttons & PSP_CTRL_UP){
-					selposconfig--;
-					if(selposconfig < 0)selposconfig=0;
-				}
-				if(pad.Buttons & PSP_CTRL_DOWN){
-					selposconfig++;
-					if(selposconfig >= totalconfig -1)selposconfig=totalconfig-1;
-				}
-
-			}
-			oldPad = pad;
-		}
-
-	}
-
-}
-
-//5SM2SF
-
-
 typedef struct fname{
-	char name[256];
+		char name[256];
 }f_name;
 
 typedef struct flist{
-	f_name fname[256];
-	int cnt;
+		f_name fname[256];
+		int cnt;
 }f_list;
 
-f_list filelist;
-
-void ClearFileList(){
-	filelist.cnt =0;
-}
-
-
-int HasExtension(char *filename){
-	if(filename[strlen(filename)-4] == '.'){
-		return 1;
-	}
-	return 0;
-}
-
-
-void GetExtension(const char *srcfile,char *outext){
-	if(HasExtension((char *)srcfile)){
-		strcpy(outext,srcfile + strlen(srcfile) - 3);
-	}else{
-		strcpy(outext,"");
-	}
-}
-
 enum {
-	EXT_NDS = 1,
-	EXT_GZ = 2,
-	EXT_ZIP = 4,
-	EXT_UNKNOWN = 8,
+		EXT_NDS = 1,
+		EXT_GZ = 2,
+		EXT_ZIP = 4,
+		EXT_UNKNOWN = 8,
 };
 
 const struct {
-	char *szExt;
-	int nExtId;
+		char *szExt;
+		int nExtId;
 } stExtentions[] = {
-	{"nds",EXT_NDS},
-//	{"gz",EXT_GZ},
-//	{"zip",EXT_ZIP},
-	{NULL, EXT_UNKNOWN}
+		{"nds",EXT_NDS},
+		{NULL, EXT_UNKNOWN}
 };
 
-int getExtId(const char *szFilePath) {
-	char *pszExt;
+configP configparms[30];
+f_list filelist;
 
-	if ((pszExt = strrchr(szFilePath, '.'))) {
-		pszExt++;
-		int i;
-		for (i = 0; stExtentions[i].nExtId != EXT_UNKNOWN; i++) {
-			if (!stricmp(stExtentions[i].szExt,pszExt)) {
-				return stExtentions[i].nExtId;
-			}
-		}
-	}
-
-	return EXT_UNKNOWN;
-}
-
-void GetFileList(const char *root)
-{
-	int dfd;
-	dfd = sceIoDopen(root);
-	if(dfd > 0){
-		SceIoDirent dir;
-		while(sceIoDread(dfd, &dir) > 0)
-		{
-			if(dir.d_stat.st_attr & FIO_SO_IFDIR)
-			{
-				//directories
-			}
-			else
-			{				
-				if(getExtId(dir.d_name)!= EXT_UNKNOWN){
-				strcpy(filelist.fname[filelist.cnt].name,dir.d_name);
-				filelist.cnt++;
-				}
-			
-			}
-		}
-		sceIoDclose(dfd);
-	}
-}
-
+int totalconfig=0;
+int selposconfig=0;
+int frameposconfig=0;
+int langposconfig=0;
 int selpos=0;
-void DisplayFileList()
-{
-	int c,x,y;
-	x=28; y=32;
-	for (c=0;c<filelist.cnt;c++){
-		if(selpos == c){
-			pspDebugScreenSetTextColor(0x0000ffff);
-		}else{
-			pspDebugScreenSetTextColor(0xffffffff);
-		}
-		
-			pspDebugScreenPrintf("%s\n",filelist.fname[c].name);
-			y+=10;
-			
-	}
-}
 
 char app_path[128];
 char romname[256];
 
-void DSEmuGui(char *path,char *out)
-{
-	//int done =0;
-	//char ext[4];
-	char tmp[256];
-	//char *slash;
-	//char appsave[1024];
+void InitConfigParms() {
+		int c=0;
 
-	SceCtrlData pad,oldPad;
-	
-	ClearFileList();
+	  strcpy(configparms[c].name,"Screen <--> SWAP");
+		swap = configparms[c].var;
+		c++;
+		strcpy(configparms[c].name,"Show FPS");
+		showfps = configparms[c].var;
+		c++;
+		strcpy(configparms[c].name,"Enable Audio");
+		enable_sound = configparms[c].var;
+		c++;
+		strcpy(configparms[c].name,"frameskip");
+		frameskip = configparms[c].var;
+		c++;
+	  strcpy(configparms[c].name,"language");
+		lang = configparms[c].var;
+		c++;
 
-	pspDebugScreenSetXY(0,0);
-	/*
-    strcpy(app_path, path);
-	slash = strrchr(app_path, '/');
-	if(slash == NULL)
-		{
-			printf("Could not find last slash Sorry \n");
-		}
-	slash++;
-	*slash = 0;
-*/
-	getcwd(app_path,256);
-
-	sprintf(tmp,"%s/NDSROM",app_path);
-
-
-	GetFileList(tmp);
-
-	int cnt;
-	long tm;
-	for(cnt=0;cnt<100;cnt++)pspDebugScreenPrintf("\n");
-	while(1){
-		sceDisplayWaitVblankStart();
-		pspDebugScreenSetTextColor(0xffffffff);
-		pspDebugScreenSetXY(1, 0);
-		pspDebugScreenPrintf("\n");
-		pspDebugScreenPrintf("\n");
-		pspDebugScreenPrintf("  Welcome to %s Based on *DeSmuMe*\n\n");
-		pspDebugScreenPrintf("  press CROSS to launch your game \n\n");
-		pspDebugScreenPrintf("  press SQUARE now to exit :=X \n\n");
-		pspDebugScreenPrintf("  ROM path %s \n\n",tmp);
-		DisplayFileList();
-		if(sceCtrlPeekBufferPositive(&pad, 1))
-		{
-			if (pad.Buttons != oldPad.Buttons)
-			{
-				if(pad.Buttons & PSP_CTRL_SQUARE){
-			      sceKernelExitGame();
-				}
-
-				if(pad.Buttons & PSP_CTRL_CROSS)
-				{
-				 sprintf(out,"%s/%s",tmp,filelist.fname[selpos].name);			
-						break;
-					
-				}
-			
-				if(pad.Buttons & PSP_CTRL_UP){
-					selpos--;
-					if(selpos < 0)selpos=0;
-				}
-				if(pad.Buttons & PSP_CTRL_DOWN){
-					selpos++;
-					if(selpos >= filelist.cnt -1)selpos=filelist.cnt-1;
-				}
-
-			}
-			oldPad = pad;
-		}
-
-	}
+		totalconfig = c;
 }
 
+void DisplayConfigParms() {
+		int c;
+
+		for (c=0;c<totalconfig;c++){
+	  		if (selposconfig == c) {
+						pspDebugScreenSetTextColor(0x0000ffff); // Yellow
+				} else {
+						pspDebugScreenSetTextColor(0xffffffff); // red
+				}
+
+				pspDebugScreenPrintf("  %s :  %d\n",configparms[c].name,configparms[c].var);
+		}
+}
+
+void DoConfig() {
+		int done=0;
+		int cnt;
+
+		SceCtrlData pad,oldPad;
+		pspDebugScreenSetXY(0,0);
+
+		for (cnt=0;cnt<100;cnt++) //pspDebugScreenPrintf("\n"); //? //?
+		while (!done) {
+				sceDisplayWaitVblankStart();
+
+				pspDebugScreenSetTextColor(0xffffffff);
+				pspDebugScreenSetXY(0, 3);
+
+				pspDebugScreenPrintf("\n");
+				pspDebugScreenPrintf("\n");
+				pspDebugScreenPrintf("  DSonPSP configuration\n\n");
+				pspDebugScreenPrintf("  When you have finished your configuration, press START \n\n");
+				pspDebugScreenPrintf("  Lang config: 0 = JAP, 1 = ENG, 2 = FRE, 3 = GER,\n");
+				pspDebugScreenPrintf("  4 = ITA, 5 = SPA, 6 = CHI, 7 = RES\n");
+				pspDebugScreenPrintf("\n");
+				pspDebugScreenPrintf("\n");
+
+				InitConfigParms();
+				DisplayConfigParms();
+
+				if (sceCtrlPeekBufferPositive(&pad, 1)) {
+						if (pad.Buttons != oldPad.Buttons) {
+                if(pad.Buttons & PSP_CTRL_LEFT) {
+		   							if(strcmp(configparms[selposconfig].name,"language")== 0) {
+												configparms[selposconfig].var = langposconfig--;
+												if(langposconfig == -1 )langposconfig = 0;
+										} else {
+												if(strcmp(configparms[selposconfig].name,"frameskip")== 0) {
+														configparms[selposconfig].var = frameposconfig--;
+														if(frameposconfig == -1 )frameposconfig = 0;
+												} else {
+														configparms[selposconfig].var = 0;
+				 								}
+										}
+								}
+
+						if(pad.Buttons & PSP_CTRL_RIGHT) {
+								if(strcmp(configparms[selposconfig].name,"language")== 0) {
+										configparms[selposconfig].var = langposconfig++;
+										if(langposconfig == 8 )langposconfig = 7;
+								} else
+										if(strcmp(configparms[selposconfig].name,"frameskip")== 0) {
+												configparms[selposconfig].var = frameposconfig++;
+                				if(frameposconfig == 10)frameposconfig = 9;
+										} else {
+                				configparms[selposconfig].var =1;
+										}
+						}
+
+					if(pad.Buttons & PSP_CTRL_START){
+							done = 1; //delay
+							break;
+					}
+					if(pad.Buttons & PSP_CTRL_UP){
+							selposconfig--;
+							if(selposconfig < 0)selposconfig=0;
+					}
+					if(pad.Buttons & PSP_CTRL_DOWN){
+							selposconfig++;
+							if(selposconfig >= totalconfig -1)selposconfig=totalconfig-1;
+					}
+
+					}
+			  oldPad = pad;
+				}
+		}
+}
+
+//5SM2SF // The f*ck is this?
+
+void ClearFileList() {
+		filelist.cnt =0;
+}
+
+int HasExtension(char *filename) {
+		if(filename[strlen(filename)-4] == '.'){
+			return 1;
+		}
+		return 0;
+}
+
+void GetExtension(const char *srcfile,char *outext) {
+		if(HasExtension((char *)srcfile)){
+				strcpy(outext,srcfile + strlen(srcfile) - 3);
+		} else {
+				strcpy(outext,"");
+		}
+}
+
+int getExtId(const char *szFilePath) {
+		char *pszExt;
+
+		if ((pszExt = strrchr(szFilePath, '.'))) {
+				pszExt++;
+				int i;
+
+				for (i = 0; stExtentions[i].nExtId != EXT_UNKNOWN; i++) {
+						if (!stricmp(stExtentions[i].szExt,pszExt)) {
+								return stExtentions[i].nExtId;
+						}
+				}
+		}
+
+		return EXT_UNKNOWN;
+}
+
+void GetFileList(const char *root) {
+		int dfd;
+		dfd = sceIoDopen(root);
+
+		if (dfd > 0) {
+				SceIoDirent dir;
+
+				while (sceIoDread(dfd, &dir) > 0) {
+						if(dir.d_stat.st_attr & FIO_SO_IFDIR) {
+								//directories
+						} else {
+								if (getExtId(dir.d_name)!= EXT_UNKNOWN) {
+										strcpy(filelist.fname[filelist.cnt].name,dir.d_name);
+										filelist.cnt++;
+								}
+						}
+				}
+
+		sceIoDclose(dfd);
+
+		}
+}
+
+void DisplayFileList() {
+		int c, x, y;
+		x=28; y=32;
+		for (c=0;c<filelist.cnt;c++) {
+				if (selpos == c) {
+						pspDebugScreenSetTextColor(0x0000ffff);
+				} else {
+						pspDebugScreenSetTextColor(0xffffffff);
+				}
+
+				pspDebugScreenPrintf("%s\n",filelist.fname[c].name);
+				y+=10;
+		}
+}
+
+void DSEmuGui(char *path,char *out) {
+		char tmp[256];
+		SceCtrlData pad, oldPad;
+
+		ClearFileList();
+		pspDebugScreenSetXY(0,0);
+
+		getcwd(app_path,256);
+		sprintf(tmp,"%s/NDSROM",app_path);
+		GetFileList(tmp);
+
+		int cnt;
+		long tm;
+		for(cnt=0;cnt<100;cnt++)pspDebugScreenPrintf("\n");
+		while(1) {
+				sceDisplayWaitVblankStart();
+
+				pspDebugScreenSetTextColor(0xffffffff);
+				pspDebugScreenSetXY(1, 0);
+
+				pspDebugScreenPrintf("\n");
+				pspDebugScreenPrintf("\n");
+				pspDebugScreenPrintf("  Welcome to DSonPSP | Based on *DeSmuMe*\n\n");
+				pspDebugScreenPrintf("  press CROSS to launch your game\n\n");
+				pspDebugScreenPrintf("  press SQUARE now to exit :=X\n\n");
+				pspDebugScreenPrintf("  ROM path %s \n\n",tmp);
+
+				DisplayFileList();
+
+				if (sceCtrlPeekBufferPositive(&pad, 1)) {
+						if (pad.Buttons != oldPad.Buttons) {
+								if (pad.Buttons & PSP_CTRL_SQUARE) {
+			      				sceKernelExitGame();
+								}
+
+								if (pad.Buttons & PSP_CTRL_CROSS) {
+				 						sprintf(out,"%s/%s",tmp,filelist.fname[selpos].name);
+										break;
+								}
+
+								if (pad.Buttons & PSP_CTRL_UP) {
+										selpos--;
+										if(selpos < 0)selpos=0;
+								}
+
+								if (pad.Buttons & PSP_CTRL_DOWN) {
+										selpos++;
+										if(selpos >= filelist.cnt -1)selpos=filelist.cnt-1;
+								}
+						}
+						oldPad = pad;
+				}
+		}
+}
--- PSP/main.c
+++ PSP/main.c
@@ -18,12 +18,20 @@
  * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
+
 #include <pspkernel.h>
 #include <pspdebug.h>
 #include <pspdisplay.h>
 #include <pspctrl.h>
 #include <pspvfpu.h>
+#include <pspgu.h>
+#include <pspgum.h>
+#include <psprtc.h>
 #include <stdio.h>
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+
 #include "video.h"
 #include "MMU.h"
 #include "NDSSystem.h"
@@ -35,8 +43,9 @@
 #include "gdbstub.h"
 #include "FrontEnd.h"
 #include "Version.h"
-#include <GL/gl.h>
-#include <GL/glu.h>
+
+#define NUM_FRAMES_TO_TIME 15
+#define FPS_LIMITER_FRAME_PERIOD 8
 
 PSP_MODULE_INFO("DSOnPSP", 0, 1, 1);
 PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_VFPU | PSP_THREAD_ATTR_USER);
@@ -46,18 +55,13 @@ NDS_header * header;
 volatile BOOL execute = FALSE;
 
 static float nds_screen_size_ratio = 1.0f;
-
-#define NUM_FRAMES_TO_TIME 15
-
-#define FPS_LIMITER_FRAME_PERIOD 8
-
 static SDL_Surface * surface;
 
-/* Flags to pass to SDL_SetVideoMode */
+// Flags to pass to SDL_SetVideoMode
 static int sdl_videoFlags = 0;
 static int sdl_quit = 0;
 static u16 keypad;
-  
+
 u8 *GPU_vram[512*192*4];
 u8 *GPU_mergeA[256*192*4];
 u8 *GPU_mergeB[256*192*4];
@@ -68,27 +72,21 @@ u32 fps_previous_time = 0;
 u32 fps_temp_time;
 u32 opengl_2d = 0;
 
-
-#include <pspgu.h>
-#include <pspgum.h>
-#include <psprtc.h>
-
 SoundInterface_struct *SNDCoreList[] = {
-  &SNDDummy,
-  &SNDFile,
-  &SNDSDL,
-  NULL
+  	&SNDDummy,
+  	&SNDFile,
+  	&SNDSDL,
+  	NULL
 };
 
 GPU3DInterface *core3DList[] = {
-&gpu3DNull
+		&gpu3DNull
 };
 
 int savetype=MC_TYPE_AUTODETECT;
 u32 savesize=1;
 
-
-const char * save_type_names[] = {
+const char *save_type_names[] = {
     "Autodetect",
     "EEPROM 4kbit",
     "EEPROM 64kbit",
@@ -99,186 +97,156 @@ const char * save_type_names[] = {
     NULL
 };
 
+void Gu_draw() {
+	  int i;
+  	PspImage *image;
+  	u16 *src, *dst,*dstA,*dstB;
+    src = (u16*)GPU_screen;
+  	dstA = (u16*)GPU_mergeA;
+  	dstB = (u16*)GPU_mergeB;
+    dst = (u16*)GPU_vram;
 
-void Gu_draw()
-{
-	 int i;
-	 PspImage *image;
-	 u16 *src, *dst,*dstA,*dstB;
-     src = (u16*)GPU_screen;
-	 dstA = (u16*)GPU_mergeA;
-	 dstB = (u16*)GPU_mergeB;
-     dst = (u16*)GPU_vram;
-   
-	 for(i=0; i < 256*192; i++)
-	 { 
-	 dstA[i] = src[i];           // MainScreen Hack
-	 dstB[i] = src[(256*192)+i]; // SubScreen Hack
-	 }
-    if(swap)
-    GuImageDirect(dstB,dstA,0,0,256,192, 0,40, 256, 192);
-	else
-    GuImageDirect(dstA,dstB,0,0,256,192, 0,40, 256, 192);
+  	for (i=0; i < 256*192; i++) {
+        dstA[i] = src[i];           // MainScreen Hack
+	      dstB[i] = src[(256*192)+i]; // SubScreen Hack
+	  }
+    if (swap) {
+        GuImageDirect(dstB,dstA,0,0,256,192, 0,40, 256, 192);
+	  } else {
+        GuImageDirect(dstA,dstB,0,0,256,192, 0,40, 256, 192);
+    }
 }
 
-/* Exit callback */
-int exit_callback(int arg1, int arg2, void *common)
-{
-	sceKernelExitGame();
-
-	return 0;
+// Exit callback
+int exit_callback(int arg1, int arg2, void *common) {
+	  sceKernelExitGame();
+	  return 0;
 }
 
-/* Callback thread */
-int CallbackThread(SceSize args, void *argp)
-{
-	int cbid;
+// Callback thread
+int CallbackThread(SceSize args, void *argp) {
+		int cbid;
 
-	cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
-	sceKernelRegisterExitCallback(cbid);
+		cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
+		sceKernelRegisterExitCallback(cbid);
 
-	sceKernelSleepThreadCB();
+		sceKernelSleepThreadCB();
 
-	return 0;
+		return 0;
 }
 
-/* Sets up the callback thread and returns its thread id */
-int SetupCallbacks(void)
-{
-	int thid = 0;
+// Sets up the callback thread and returns its thread id
+int SetupCallbacks(void) {
+		int thid = 0;
 
-	thid = sceKernelCreateThread("update_thread", CallbackThread, 0x11, 0xFA0, 0, 0);
-	if(thid >= 0)
-	{
-		sceKernelStartThread(thid, 0, 0);
-	}
+		thid = sceKernelCreateThread("update_thread", CallbackThread, 0x11, 0xFA0, 0, 0);
+		if (thid >= 0) {
+				sceKernelStartThread(thid, 0, 0);
+		}
 
-	return thid;
+		return thid;
 }
 
-  int cycles;
+void DSonpspExec() {
+		sdl_quit = process_ctrls_events( &keypad, NULL, nds_screen_size_ratio);
 
-void DSonpspExec()
-{  
-
-	sdl_quit = process_ctrls_events( &keypad, NULL, nds_screen_size_ratio);
-    
-	
     // Update mouse position and click
     if(mouse.down) {
-		NDS_setTouchPos(mouse.x, mouse.y);
-	}
-	
-    if(mouse.click)
-      { 
+				NDS_setTouchPos(mouse.x, mouse.y);
+		}
+
+    if(mouse.click) {
         NDS_releasTouch();
         mouse.click = FALSE;
+    }
+
+		update_keypad(keypad); // Update keypad
+		NDS_exec(FALSE);
+
+		if (enable_sound) {
+				SPU_Emulate();
+		}
+}
+
+int main(SceSize args, void *argp) {
+  	int f;
+  	struct armcpu_memory_iface *arm9_memio = &arm9_base_memory_iface;
+  	struct armcpu_memory_iface *arm7_memio = &arm7_base_memory_iface;
+  	struct armcpu_ctrl_iface *arm9_ctrl_iface;
+	  struct armcpu_ctrl_iface *arm7_ctrl_iface;
+	  char rom_filename[256];
+
+  	pspDebugScreenInitEx((void*)(0x44000000),PSP_DISPLAY_PIXEL_FORMAT_5551, 1);
+    scePowerSetClockFrequency(333, 333, 166); // Overclock PSP CPUs
+
+    SetupCallbacks();
+
+    // this holds some info about our display
+    const SDL_VideoInfo *videoInfo;
+
+    SceCtrlData pad;
+
+    DSEmuGui(argp,rom_filename);
+
+    pspDebugScreenClear();
+    DoConfig();
+    pspDebugScreenClear();
+
+    cflash_disk_image_file = NULL;
+
+    GuInit();
+
+    // the firmware settings
+    struct NDS_fw_config_data fw_config;
+
+    NDS_FillDefaultFirmwareConfigData(&fw_config);
+    NDS_Init(arm9_memio, &arm9_ctrl_iface,
+        arm7_memio, &arm7_ctrl_iface);
+
+    // Create the dummy firmware
+    NDS_CreateDummyFirmware( &fw_config);
+
+    if (enable_sound) {
+        SPU_ChangeSoundCore(SNDCORE_SDL, 735 * 4);
+    }
+
+    mmu_select_savetype(savetype, &savetype, &savesize);
+
+    if (NDS_LoadROM(rom_filename, MC_TYPE_AUTODETECT, 1, cflash_disk_image_file) < 0) {
+        SceCtrlData pad;
+	      printf("Error loading %s!\n", rom_filename);
+	      sceKernelDelayThread(100000);
+
+	      do {
+            sceCtrlReadBufferPositive(&pad, 1);
+	      } while (pad.Buttons == 0);
+	      sceKernelExitGame();
+    }
+
+    execute = TRUE;
+
+    while(!sdl_quit) {
+      // Look for queued events and update keypad status
+	    if (frameskip > 0) {
+	        for(f= 0; f<frameskip; f++) DSonpspExec();
+	    } else {
+          DSonpspExec();
       }
 
-	update_keypad(keypad);     /* Update keypad */
+      Gu_draw();
 
-    //cycles = NDS_exec((560190<<1)-cycles,FALSE);
-	NDS_exec(FALSE);
+      if (showfps) {
+          FPS();
+      }
 
-	if ( enable_sound) {
-		SPU_Emulate();
-	}
+	    if (enable_sound) {
+		      SPU_Emulate();
+	    }
+    }
 
-}
-
-
-int main(SceSize args, void *argp)
-{
-  int f;
-  struct armcpu_memory_iface *arm9_memio = &arm9_base_memory_iface;
-  struct armcpu_memory_iface *arm7_memio = &arm7_base_memory_iface;
-  struct armcpu_ctrl_iface *arm9_ctrl_iface;
-  struct armcpu_ctrl_iface *arm7_ctrl_iface;
-  char rom_filename[256];
-
-  pspDebugScreenInitEx((void*)(0x44000000),PSP_DISPLAY_PIXEL_FORMAT_5551, 1);
-  //Overclock 
-  scePowerSetClockFrequency(333, 333, 166);
-  
-  SetupCallbacks();
- 
-  /* this holds some info about our display */
-  const SDL_VideoInfo *videoInfo;
-
-  SceCtrlData pad;
- 
-  DSEmuGui(argp,rom_filename);
-    
-  pspDebugScreenClear(); 
-  
-  DoConfig();
-
-  pspDebugScreenClear();
-
-  cflash_disk_image_file = NULL;
-
-
-  GuInit();
-
-  /* the firmware settings */
-  struct NDS_fw_config_data fw_config;
-
-  NDS_FillDefaultFirmwareConfigData(&fw_config); 
-
-
-  NDS_Init( arm9_memio, &arm9_ctrl_iface,
-            arm7_memio, &arm7_ctrl_iface);
-  
-
-    /* Create the dummy firmware */
-  NDS_CreateDummyFirmware( &fw_config);
-
-  if ( enable_sound) {
-    SPU_ChangeSoundCore(SNDCORE_SDL, 735 * 4);
-  }
-  
-  //rom_filename = "rom.nds";
- mmu_select_savetype(savetype, &savetype, &savesize);
-
-
-if (NDS_LoadROM( rom_filename  , MC_TYPE_AUTODETECT, 1, cflash_disk_image_file) < 0) {
-    SceCtrlData pad;
-	printf("Error loading %s\n", rom_filename);
-	sceKernelDelayThread(100000);
-	do { sceCtrlReadBufferPositive(&pad, 1);
-	} while (pad.Buttons == 0);
-	sceKernelExitGame();
-  }
-
-  execute = TRUE;
-  
-
-//    SDL_ShowCursor(SDL_DISABLE);
-
- 
-  
-  while(!sdl_quit) {
-  	
-	 // Look for queued events and update keypad status
-	if(frameskip > 0)
-	for(f= 0; f<frameskip; f++) DSonpspExec();
-	else DSonpspExec();
-
-    Gu_draw();
-
-    if(showfps)
-     FPS();
-
-
-	if ( enable_sound) {
-		SPU_Emulate();
-	}
-
-}
-
-  SDL_Quit();
-  NDS_DeInit();
-  return 0;
+    SDL_Quit();
+    NDS_DeInit();
+    return 0;
 }
 
 /*
