ad56387c6fdc3095fde5651dc4842a8e52425ed1
TeilzeitTaco <42586657+TeilzeitTaco@users.noreply.github.com>
2019-02-11 16:41:19 +0100

    stuff

--- PSP/Gudraw.c
+++ PSP/Gudraw.c
@@ -36,32 +36,32 @@ static unsigned int __attribute__((aligned(16))) List[512*192*4]; /* TODO: ? */
 void FPS() {
     float curr_fps = 1.0f / curr_ms;
 
-	  pspDebugScreenSetXY(0,5);
-	  pspDebugScreenPrintf("FPS %d.%03d V0.6",(int)curr_fps,(int)((curr_fps-(int)curr_fps) * 1000.0f));
+    pspDebugScreenSetXY(0,5);
+    pspDebugScreenPrintf("FPS %d.%03d V0.6",(int)curr_fps,(int)((curr_fps-(int)curr_fps) * 1000.0f));
     gettimeofday(&time_slices[val & 15],0);
 
-		val++;
+    val++;
 
-		if (!(val & 15)) {
-		  	struct timeval last_time = time_slices[0];
-		  	unsigned int i;
+    if (!(val & 15)) {
+        struct timeval last_time = time_slices[0];
+        unsigned int i;
 
-			  curr_ms = 0;
-		  	for (i = 1; i < 16; ++i) {
-				    struct timeval curr_time = time_slices[i];
+        curr_ms = 0;
+        for (i = 1; i < 16; ++i) {
+            struct timeval curr_time = time_slices[i];
 
-				    if (last_time.tv_usec > curr_time.tv_usec) {
-					      curr_time.tv_sec++;
-					      curr_time.tv_usec-=1000000;
-				    }
+            if (last_time.tv_usec > curr_time.tv_usec) {
+                curr_time.tv_sec++;
+                curr_time.tv_usec-=1000000;
+            }
 
-				    curr_ms += ((curr_time.tv_usec-last_time.tv_usec) + (curr_time.tv_sec-last_time.tv_sec) * 1000000) * (1.0f/1000000.0f);
+            curr_ms += ((curr_time.tv_usec-last_time.tv_usec) + (curr_time.tv_sec-last_time.tv_sec) * 1000000) * (1.0f/1000000.0f);
 
-				    last_time = time_slices[i];
-			  }
-			  curr_ms /= 15.0f;
-		}
-		sceDisplayWaitVblankStart();
+            last_time = time_slices[i];
+        }
+        curr_ms /= 15.0f;
+    }
+    sceDisplayWaitVblankStart();
 }
 
 void* fbp0;
@@ -75,11 +75,11 @@ void GuInit() {
     TexColor = GU_COLOR_5551;
     TexFilter = GU_NEAREST;
 
-     int size;
+    int size;
 
-	  fbp0 = getStaticVramBuffer(BUF_WIDTH,SCR_HEIGHT,GU_PSM_8888);
-  	fbp1 = getStaticVramBuffer(BUF_WIDTH,SCR_HEIGHT,GU_PSM_8888);
-  	zbp = getStaticVramBuffer(BUF_WIDTH,SCR_HEIGHT,GU_PSM_4444);
+    fbp0 = getStaticVramBuffer(BUF_WIDTH,SCR_HEIGHT,GU_PSM_8888);
+    fbp1 = getStaticVramBuffer(BUF_WIDTH,SCR_HEIGHT,GU_PSM_8888);
+    zbp = getStaticVramBuffer(BUF_WIDTH,SCR_HEIGHT,GU_PSM_4444);
 
     sceGuInit();
 
@@ -124,75 +124,75 @@ void pspVideoWaitVSync() {
 
 
 void GuImageDirect(u16* imageA,u16* imageB,int VX,int VY,int Width,int Height, int dx, int dy, int dw, int dh) {
-	  sceKernelDcacheWritebackAll();
+    sceKernelDcacheWritebackAll();
 
-	  sceGuStart(GU_DIRECT, List);
+    sceGuStart(GU_DIRECT, List);
 
-	  sceGuScissor(dx, dy, dx + dw, dy + dh);
+    sceGuScissor(dx, dy, dx + dw, dy + dh);
 
     if (dw == Width && dh == Height) {
-	      if(vertical) {
+        if(vertical) {
             sceGuCopyImage(PixelFormat,
-            VX, VY,
-            Width, Height,
-            Width, imageA, dx, dy,
-            BUF_WIDTH, (void *)(VRAM_START + (u32)fbp1));
-            sceGuCopyImage(PixelFormat,
-            VX, VY,
-            Width, Height,
-            Width, imageB, Width, dy,
-            BUF_WIDTH, (void *)(VRAM_START + (u32)fbp1));
-            sceGuTexSync();
-	      } else {
-            sceGuCopyImage(PixelFormat,
-            VX, VY,
-            240, Height,
-            Width, imageA, dx, dy,
-            BUF_WIDTH, (void *)(VRAM_START + (u32)fbp1));
-            sceGuCopyImage(PixelFormat,
-            VX, VY,
-            240, Height,
-            Width, imageB, 240, dy,
-            BUF_WIDTH, (void *)(VRAM_START + (u32)fbp1));
-            sceGuTexSync();
-        }
-    } else {
-        sceGuEnable(GU_TEXTURE_2D);
-        sceGuTexMode(TexFormat, 0, 0, 0);
-        sceGuTexImage(0, Width, Width, Width, imageA);
-        sceGuTexFunc(GU_TFX_REPLACE, GU_TCC_RGBA);
-        sceGuTexFilter(TexFilter, TexFilter);
+                VX, VY,
+                Width, Height,
+                Width, imageA, dx, dy,
+                BUF_WIDTH, (void *)(VRAM_START + (u32)fbp1));
+                sceGuCopyImage(PixelFormat,
+                    VX, VY,
+                    Width, Height,
+                    Width, imageB, Width, dy,
+                    BUF_WIDTH, (void *)(VRAM_START + (u32)fbp1));
+                    sceGuTexSync();
+                } else {
+                    sceGuCopyImage(PixelFormat,
+                        VX, VY,
+                        240, Height,
+                        Width, imageA, dx, dy,
+                        BUF_WIDTH, (void *)(VRAM_START + (u32)fbp1));
+                        sceGuCopyImage(PixelFormat,
+                            VX, VY,
+                            240, Height,
+                            Width, imageB, 240, dy,
+                            BUF_WIDTH, (void *)(VRAM_START + (u32)fbp1));
+                            sceGuTexSync();
+                        }
+                    } else {
+                        sceGuEnable(GU_TEXTURE_2D);
+                        sceGuTexMode(TexFormat, 0, 0, 0);
+                        sceGuTexImage(0, Width, Width, Width, imageA);
+                        sceGuTexFunc(GU_TFX_REPLACE, GU_TCC_RGBA);
+                        sceGuTexFilter(TexFilter, TexFilter);
 
-        struct Vertex* vertices;
-        int start, end, sc_end, slsz_scaled;
-        slsz_scaled = ceil((float)dw * (float)SLICE_SIZE) / (float)Width;
+                        struct Vertex* vertices;
+                        int start, end, sc_end, slsz_scaled;
+                        slsz_scaled = ceil((float)dw * (float)SLICE_SIZE) / (float)Width;
 
-        for (start = VX, end = VX + Width, sc_end = dx + dw; start < end; start += SLICE_SIZE, dx += slsz_scaled) {
-            vertices = (struct Vertex*)sceGuGetMemory(2 * sizeof(struct Vertex));
+                        for (start = VX, end = VX + Width, sc_end = dx + dw; start < end; start += SLICE_SIZE, dx += slsz_scaled) {
+                            vertices = (struct Vertex*)sceGuGetMemory(2 * sizeof(struct Vertex));
 
-            vertices[0].u = start;
-            vertices[0].v = VY;
-            vertices[1].u = start + SLICE_SIZE;
-            vertices[1].v = Height + VY;
+                            vertices[0].u = start;
+                            vertices[0].v = VY;
+                            vertices[1].u = start + SLICE_SIZE;
+                            vertices[1].v = Height + VY;
 
-            vertices[0].x = dx; vertices[0].y = dy;
-            vertices[1].x = dx + slsz_scaled; vertices[1].y = dy + dh;
+                            vertices[0].x = dx; vertices[0].y = dy;
+                            vertices[1].x = dx + slsz_scaled; vertices[1].y = dy + dh;
 
-            vertices[0].color
-                = vertices[1].color
-                = vertices[0].z
-                = vertices[1].z = 0;
+                            vertices[0].color
+                            = vertices[1].color
+                            = vertices[0].z
+                            = vertices[1].z = 0;
 
-	          sceGuDrawArray(GU_SPRITES,GU_TEXTURE_16BIT|GU_COLOR_5551|GU_VERTEX_16BIT|GU_TRANSFORM_2D,2,0,vertices);
-        }
+                            sceGuDrawArray(GU_SPRITES,GU_TEXTURE_16BIT|GU_COLOR_5551|GU_VERTEX_16BIT|GU_TRANSFORM_2D,2,0,vertices);
+                        }
 
-        sceGuDisable(GU_TEXTURE_2D);
+                        sceGuDisable(GU_TEXTURE_2D);
 
-    }
+                    }
 
-    sceGuScissor(0, 0, SCR_WIDTH, SCR_HEIGHT);
-    sceGuFinish();
-    sceGuSync(0,0);
-    pspVideoSwapBuffers();
-    sceDisplayWaitVblankStart();
-}
+                    sceGuScissor(0, 0, SCR_WIDTH, SCR_HEIGHT);
+                    sceGuFinish();
+                    sceGuSync(0,0);
+                    pspVideoSwapBuffers();
+                    sceDisplayWaitVblankStart();
+                }
--- PSP/callbacks.c
+++ PSP/callbacks.c
@@ -42,8 +42,7 @@ int setupCallbacks(void) {
 	int thid = 0;
 
 	thid = sceKernelCreateThread("update_thread", callbackThread, 0x11, 0xFA0, 0, 0);
-	if(thid >= 0)
-	{
+	if(thid >= 0) {
 		sceKernelStartThread(thid, 0, 0);
 	}
 
